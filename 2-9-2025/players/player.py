4ec77b8b19a43cd3cea2e3ee83dc1154b9a4e87c
swathi3030 <swathichr2002@gmail.com>
2025-09-02 16:51:39 +0530

    players

---
+++ 2-9-2025/players/player.py
@@ -0,0 +1,20 @@
+players = [
+    {'id': 101, 'name': 'jaiswal'},
+    {'id': 102, 'name' : 'gill'}
+]
+
+print(players)
+
+player={}
+# player=dict()
+player['id'] = 103
+player['name'] = 'abhishek'
+print(player)
+players.append(player)
+print(players)
+
+for player in players:
+    if player['id'] == 103:
+        print(player)
+players_dict={101:players[0], 102:players[1], 103:players[3]}
+print(players_dict[103])
